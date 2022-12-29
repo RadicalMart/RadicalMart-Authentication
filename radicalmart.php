22e4bfb363fdeee29cb381ff225f28ab4ade4a22
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-12-29 13:39:40 +0300

    Add RadicalMart 1.0.0 Support (#3)

--- radicalmart.php
+++ radicalmart.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Express Package
  * @subpackage  plg_button_radicalmart_express
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
@@ -11,7 +11,9 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Authentication\Authentication;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 
@@ -22,7 +24,7 @@ class plgAuthenticationRadicalMart extends CMSPlugin
 	 *
 	 * @var  boolean
 	 *
-	 * @since  __DEPLOY_VERSION__
+	 * @since  1.0.0
 	 */
 	protected $autoloadLanguage = true;
 
@@ -31,7 +33,7 @@ class plgAuthenticationRadicalMart extends CMSPlugin
 	 *
 	 * @var string
 	 *
-	 * @since __DEPLOY_VERSION__
+	 * @since 1.0.0
 	 */
 	public $name = 'Radicalmart';
 
@@ -44,23 +46,14 @@ class plgAuthenticationRadicalMart extends CMSPlugin
 	 *
 	 * @return  bool True on success, false on failure.
 	 *
-	 * @since  __DEPLOY_VERSION__
+	 * @since  1.0.0
 	 */
 	public function onUserAuthenticate(&$credentials, $options, &$response)
 	{
-		// Check password
-		if (empty($credentials['password']))
-		{
-			$response->status        = JAuthentication::STATUS_FAILURE;
-			$response->error_message = Text::_('JGLOBAL_AUTH_EMPTY_PASS_NOT_ALLOWED');
-
-			return false;
-		}
-
 		// Check username
 		if (empty($credentials['username']))
 		{
-			$response->status        = JAuthentication::STATUS_FAILURE;
+			$response->status        = Authentication::STATUS_FAILURE;
 			$response->error_message = Text::_('JGLOBAL_AUTH_NO_USER');
 
 			return false;
@@ -68,35 +61,89 @@ class plgAuthenticationRadicalMart extends CMSPlugin
 
 		try
 		{
-			if (!empty(ComponentHelper::getComponent('com_radicalmart_express')->id))
+			$find = false;
+			if (!empty(ComponentHelper::getComponent('com_radicalmart')->id))
 			{
+				$find          = true;
+				$userHelper    = '\Joomla\Component\RadicalMart\Administrator\Helper\UserHelper';
+				$pluginsHelper = '\Joomla\Component\RadicalMart\Administrator\Helper\PluginsHelper';
+			}
+			elseif (!empty(ComponentHelper::getComponent('com_radicalmart_express')->id))
+			{
+				$find = true;
 				JLoader::register('RadicalMartHelperUser',
 					JPATH_ADMINISTRATOR . '/components/com_radicalmart_express/helpers/user.php');
 				JLoader::register('RadicalMartHelperPlugins',
 					JPATH_ADMINISTRATOR . '/components/com_radicalmart_express/helpers/plugins.php');
+				$userHelper    = 'RadicalMartHelperUser';
+				$pluginsHelper = 'RadicalMartHelperPlugins';
 			}
-			else
+
+			if (!$find)
 			{
-				JLoader::register('RadicalMartHelperUser',
-					JPATH_ADMINISTRATOR . '/components/com_radicalmart/helpers/user.php');
-				JLoader::register('RadicalMartHelperPlugins',
-					JPATH_ADMINISTRATOR . '/components/com_radicalmart/helpers/plugins.php');
+				throw new Exception(Text::_('PLG_AUTHENTICATION_RADICALMART_ERROR_COMPONENT_NOT_FOUND'));
 			}
 
 			// Prepare data
 			$data = array(
 				'username' => $credentials['username'],
 				'email'    => $credentials['username'],
-				'phone'    => RadicalMartHelperUser::cleanPhone($credentials['username']),
+				'phone'    => $userHelper::cleanPhone($credentials['username']),
 			);
-			if (!$user = RadicalMartHelperUser::findUser($data)) throw new Exception(Text::_('JGLOBAL_AUTH_NO_USER'));
 
-			// Check password
+			if (!$user = $userHelper::findUser($data))
+			{
+				throw new Exception(Text::_('JGLOBAL_AUTH_NO_USER'));
+			}
 			$credentials['username'] = $user->username;
-			RadicalMartHelperPlugins::triggerPlugin('authentication', 'joomla', 'onUserAuthenticate',
-				array($credentials, $options, &$response));
 
-			if ($response->status == JAuthentication::STATUS_FAILURE) throw new Exception($response->error_message);
+			// Get authentication type
+			$type = (!empty($credentials['type'])) ? $credentials['type'] : 'password';
+			if ($type === 'rma_code')
+			{
+				// Authentication by code
+				$code = (!empty($credentials['rma_code'])) ? trim($credentials['rma_code']) : '';
+				if (empty($code))
+				{
+					throw new Exception(Text::_('PLG_AUTHENTICATION_RADICALMART_ERROR_EMPTY_CODE'));
+				}
+
+				$session = trim(Factory::getApplication()->getUserState('rma_code', ''));
+				if (empty($session))
+				{
+					throw new Exception(Text::_('PLG_AUTHENTICATION_RADICALMART_ERROR_EMPTY_SESSION'));
+				}
+
+				if ($session !== $code)
+				{
+					throw new Exception(Text::_('PLG_AUTHENTICATION_RADICALMART_ERROR_CODE_INVALID'));
+				}
+
+				// Set response data
+				$response->email         = $user->email;
+				$response->fullname      = $user->name;
+				$response->language      = (Factory::getApplication()->isClient('administrator'))
+					? $user->getParam('admin_language') : $user->getParam('language');
+				$response->status        = Authentication::STATUS_SUCCESS;
+				$response->error_message = '';
+			}
+			else
+			{
+				// Authentication by password
+				if (empty($credentials['password']))
+				{
+					throw new Exception(Text::_('JGLOBAL_AUTH_EMPTY_PASS_NOT_ALLOWED'));
+				}
+
+				// Check password
+				$pluginsHelper::triggerPlugin('authentication', 'joomla', 'onUserAuthenticate',
+					[$credentials, $options, &$response]);
+
+				if ($response->status == JAuthentication::STATUS_FAILURE)
+				{
+					throw new Exception($response->error_message);
+				}
+			}
 
 			return true;
 		}
